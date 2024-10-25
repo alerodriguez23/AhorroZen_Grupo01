bdb54127ab91c4c07706a79bbcfe3c4e91446702
alerodriguez23 <jenniffer.rodriguez@catolica.edu.sv>
2024-10-25 14:46:15 -0600

    integracion de pantalla para categorias

--- app/src/main/java/sv/edu/catolica/ahorrozen_grupo01/categorias.java
+++ app/src/main/java/sv/edu/catolica/ahorrozen_grupo01/categorias.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Calendario extends AppCompatActivity {
+public class categorias extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_calendario);
+        setContentView(R.layout.activity_categorias);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
